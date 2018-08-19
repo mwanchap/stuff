122d6071cbf56e90939f7b22a31726a27f6f903f
Matt Wanchap <matt.wanchap@cpal.com.au>
2018-08-20 06:58:39 +1000

    Removed spotify and added nuget update to prevent confirmation prompt before installing modules

--- install everything script.ps1
+++ install everything script.ps1
@@ -52,7 +52,7 @@ ScheduleStartupTask -TaskName "startup tasks" -TaskDescription "Runs startup tas
 ("IIS-WebServerRole","IIS-WebServer","IIS-CommonHttpFeatures","IIS-HttpErrors","IIS-HttpRedirect","IIS-ApplicationDevelopment","IIS-NetFxExtensibility","IIS-NetFxExtensibility45","IIS-HealthAndDiagnostics","IIS-HttpLogging","IIS-LoggingLibraries","IIS-RequestMonitor","IIS-HttpTracing","IIS-Security","IIS-URLAuthorization","IIS-RequestFiltering","IIS-IPSecurity","IIS-Performance","IIS-HttpCompressionDynamic","IIS-WebServerManagementTools","IIS-ManagementScriptingTools","IIS-IIS6ManagementCompatibility","IIS-Metabase","IIS-StaticContent","IIS-DefaultDocument","IIS-DirectoryBrowsing","IIS-WebSockets","IIS-ApplicationInit","IIS-ASPNET","IIS-ASPNET45","IIS-ASP","IIS-CGI","IIS-ISAPIExtensions","IIS-ISAPIFilter","IIS-ServerSideIncludes","IIS-CustomLogging","IIS-BasicAuthentication","IIS-HttpCompressionStatic","IIS-ManagementConsole","IIS-ManagementService","IIS-WMICompatibility","IIS-LegacyScripts","IIS-LegacySnapIn","IIS-CertProvider","IIS-WindowsAuthentication","IIS-DigestAuthentication","IIS-ClientCertificateMappingAuthentication","IIS-IISCertificateMappingAuthentication","IIS-ODBCLogging") | % { write-host $_; Enable-WindowsOptionalFeature -Online -FeatureName $_; }
 
 #other software
-choco install googlechrome.canary nodejs.install sysinternals 7zip.install firefox vlc conemu paint.net windirstat azure-cli poshgit sumatrapdf.install irfanview negativescreen sourcetree spotify kdiff3 sql-server-management-studio microsoftazurestorageexplorer TortoiseGit rdcman qmmp postman sharex winscp force-cli rescuetime ilspy grepwin -y
+choco install googlechrome.canary nodejs.install sysinternals 7zip.install firefox vlc conemu paint.net windirstat azure-cli poshgit sumatrapdf.install irfanview negativescreen sourcetree kdiff3 sql-server-management-studio microsoftazurestorageexplorer TortoiseGit rdcman qmmp postman sharex winscp force-cli rescuetime ilspy grepwin -y
 
 #sharex hotkey
 if (Test-Path "$homedir\ShareX")
@@ -68,6 +68,7 @@ if (Test-Path $attRegPath)
 }
 
 #powershell stuff
+Install-PackageProvider -Name NuGet -Force
 Install-Module "AzureRM","AzureAD","ImportExcel", "VSTeam" -Force
 Update-Help
 
